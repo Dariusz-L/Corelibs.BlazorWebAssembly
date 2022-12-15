4e6cd0efdf32933104eb54237873f8544aa0109d
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-12-16 00:01:20 +0100

    Add reusable extension

---
+++ Corelibs.BlazorWebAssembly/Corelibs.BlazorWebAssembly/BlazorWebAssemblyExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Corelibs.BlazorShared;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Corelibs.BlazorWebAssembly
+{
+    public static class BlazorWebAssemblyExtensions
+    {
+        public static void AddAuthorizationAndSignInRedirection(
+           this IServiceCollection services, string baseAddress)
+        {
+            services.AddAuthorizationAndSignInRedirection<
+                WebAuthUser, WebSignInRedirector, AccessTokenNotAvailableException, BaseAddressAuthorizationMessageHandler>(
+                baseAddress);
+        }
+    }
+}
