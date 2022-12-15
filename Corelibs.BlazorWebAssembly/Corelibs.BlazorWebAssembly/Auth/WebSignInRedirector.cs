46d4793a1ae2738c8303364f69d4a178d091891e
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-12-15 18:44:06 +0100

    Add project

---
+++ Corelibs.BlazorWebAssembly/Corelibs.BlazorWebAssembly/Auth/WebSignInRedirector.cs
@@ -0,0 +1,25 @@
+﻿using Corelibs.BlazorShared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace Corelibs.BlazorWebAssembly
+{
+    public class WebSignInRedirector : ISignInRedirector
+    {
+        private readonly NavigationManager _navigation;
+
+        public WebSignInRedirector(NavigationManager navigation)
+        {
+            _navigation = navigation;
+        }
+
+        public void Redirect(Exception exception)
+        {
+            if (exception is AccessTokenNotAvailableException accessTokenException)
+            {
+                accessTokenException.Redirect();
+                _navigation.NavigateTo(_navigation.Uri, forceLoad: true);
+            }
+        }
+    }
+}
